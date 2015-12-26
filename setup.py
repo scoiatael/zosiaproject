a19c5a812051d861004e6eae3a4045eaad76ebfa
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2015-12-26 21:45:36 +0100

    improve versioning

--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ setup(
     author_email='zosia@cs.uni.wroc.pl',
     url='http://ksi.ii.uni.wroc.pl/',
     install_requires=[
-        'Django==1.8.4',
-        'Pillow==2.9.0',
-        'pytz==2015.4',
-        'django-mailgun==0.3.0',
+        'Django<1.9',
+        'Pillow',
+        'pytz',
+        'django-mailgun',
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/',
+        'https://pypi.python.org/simple/pillow/',
+        'https://pypi.python.org/simple/pytz/',
         'https://pypi.python.org/simple/django-mailgun/',
     ],
 )
