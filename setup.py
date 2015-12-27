becc4cf05eea48e7d0fa0e19b23c47ba0fd01842
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2015-12-27 11:50:04 +0100

    Update some deps

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ from setuptools import setup
 
 setup(
     name='zosiaproject',
-    version='0.2',
+    version='0.1',
     description='OpenShift App',
     author='KSI',
     author_email='zosia@cs.uni.wroc.pl',
     url='http://ksi.ii.uni.wroc.pl/',
     install_requires=[
-        'Django<1.9',
-        'Pillow',
-        'pytz',
-        'django-mailgun',
+        'Django==1.8.7',
+        'Pillow==3.0.0',
+        'pytz==2015.7',
+        'django-mailgun==0.8.0',
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/',
