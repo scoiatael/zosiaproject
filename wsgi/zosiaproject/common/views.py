a13c1a4ee182ff62c2e99a3ff00d93db3a02d419
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2015-12-31 01:35:17 +0100

    Fixing login logic

--- wsgi/zosiaproject/common/views.py
+++ wsgi/zosiaproject/common/views.py
@@ -7,65 +7,6 @@ from .forms import *
 
 from users.models import Participant
 
-def login_view(request):
-    # hmm, I've moved too much stuff in here, it's a bit messy
-    # and HTTP_REFERER trickery should probably be better handled
-    # but it's 6 a.m. already
-    # Special thanks to my good friend, caffeine (dreamer_)
-    title = "Login"
-    user = request.user
-    reason = None # theistic code ;)
-    try:
-        get_redir = request.GET['next']
-        if user.is_authenticated():
-            return HttpResponseRedirect(get_redir)
-        referer = request.META['HTTP_REFERER']
-    except Exception:
-        referer = None
-    if not user.is_authenticated():
-        if request.POST:
-            form = LoginForm(request.POST)
-            if form.is_valid():
-                email    = form.cleaned_data['email']
-                password = form.cleaned_data['password']
-                try:
-                    usr = Participant.objects.get(email__iexact=email.lower())
-                    if usr.is_active:
-                        usr = authenticate(username=email, password=password)
-                        if usr: 
-                            login(request,usr)
-                        else:
-                            # wrong password
-                            reason = 'LOGIN_account_disabled'
-                            pass
-                    else:
-                        # user is not active
-                        reason = 'LOGIN_account_disabled'
-                        pass
-                except Exception:
-                    # user does not exit
-                    reason = 'LOGIN_account_disabled'
-                    pass
-                if not reason:
-                    try:
-                        next = request.GET['next']
-                        if next: return HttpResponseRedirect(next)
-                        if referer.endswith('/reset/done/'):
-                            greetings_resetter = True
-                            greetings_stranger = False
-                    except Exception: pass
-                    if (referer is None) or referer.endswith('/login/') or referer.endswith('/bye/') or referer.endswith('/reset/done/'):
-                        referer = '/blog/'
-                    return HttpResponseRedirect(referer)
-        else:
-            form = LoginForm()
-    # this template is used in case of:
-    # - link from activation email
-    # - unsuccesfull login
-    # - login after password reset (aka 'password recovery')
-    return render_to_response('login.html', locals())
-
-
 def thanks(request):
     user = request.user
     title = "Bye!"
--- wsgi/zosiaproject/zosiaproject/settings.py
+++ wsgi/zosiaproject/zosiaproject/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE_CLASSES = (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
